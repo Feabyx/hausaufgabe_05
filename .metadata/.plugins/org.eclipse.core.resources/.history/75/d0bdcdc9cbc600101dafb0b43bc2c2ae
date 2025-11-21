903590b31e2e4bdb4cb9ea0c9495b0d545218592
Sara <saraber@outlook.de>
2025-11-21 20:11:39 +0100

    Hausaufgabe fünf

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/75/d0bdcdc9cbc600101dafb0b43bc2c2ae
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module praesensaufgabe05 {
+}
---
+++ hausaufgabe05/src/h3/H3_main.java
@@ -0,0 +1,23 @@
+package h3;
+
+public class H3_main {
+
+    public static void main(String[] args) {
+
+        int[][] einheiten = {
+                {50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1},
+                new int[15]     
+        };
+        int input = 6279;   
+        int rest = input;   
+       
+        for (int i = 0; i < einheiten[0].length; i++) {
+            int wert = einheiten[0][i];
+
+            if (rest >= wert) {
+                einheiten[1][i] = rest / wert;   
+                rest = rest % wert;             
+            }
+        }       
+    }
+}
